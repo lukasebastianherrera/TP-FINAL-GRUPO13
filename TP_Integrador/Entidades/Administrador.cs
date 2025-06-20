8e0e6adb4d26d5813adf13560ee702c941967bbf
Valentin <sonvalen@outlook.com.ar>
2025-06-19 22:11:55 -0300

    clase administrador

---
+++ TP_Integrador/Entidades/Administrador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Administrador : Persona
+    {
+        private int id_persona
+        private bool estado;
+
+        public Administrador() { }
+
+
+        public int Id_persona { get => id_persona; set => id_persona = value; }
+        public bool Estado { get => estado; set => estado = value; }
+    }
+}
